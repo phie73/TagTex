eef545a97eb41b82faee3e22df038b3ce2b6b1d6
43_50ph14 <so_s@gmx.de>
2024-04-27 23:52:15 +0200

    cyrill letters, error in request handeling, cleanup

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import argparse
 import requests
 import json
 import re 
-import unicodedata
 import pycountry
 from collections import OrderedDict
 
@@ -16,11 +15,6 @@ parser.add_argument('-o',"--order_by",
 parser.add_argument('-od', "--print_orga_delegate",
                     help="print orga and delegate role on nametag, options y, n, default: n",
                     default='n')
-#parser.add_argument('-l',"--layout",
-#                    help="Number of nametags to place on a page,\
-#                    given by rows x columns x (h/p)\
-#                    where h is for horizontal, p for portrait mode", default="5 x 2 x p")
-# currently, layout is just handled in preamble of the document itself, independent of content
 args = parser.parse_args()
 
 # read in user args
@@ -31,8 +25,11 @@ print_orga_delegate = args.print_orga_delegate
 # get competitor information for specific competition
 res = requests.get("https://worldcubeassociation.org/api/v0/competitions/" + comp_id + "/wcif/public")
 
-# todo some checks for 200 and resonable answer
+# if wca page has issues totaly fine to just throw here, not intended to continue in this case
 res_json = res.json()
+if "error" in res_json:
+    print(res_json["error"])
+    quit()
 activities = res_json['schedule']['venues'][0]['rooms'][0]['activities']
 activityIdMap = dict()
 for activity in activities:
@@ -51,16 +48,13 @@ tex_builder = ''
 for person in persons:
     # structure {Name}{WCAID}{country}{reg id}{assignments[event & comp & scr & judge & run\\]}
     if (bool((re.compile(r'[\u0400-\u04FF]+')).search(person["name"])) == True): #cyrill letters
-        a = person["name"].split()
-        a[2] = '(\\foreignlanguage{russian}{' + a[0]
-        a[3] = a[1] + '})'
-        tmpName = ' '.join(a)
-    elif (bool((re.compile(r'[\u4e00-\u9fff\u3000-\u303f\uff00-\uffef]')).search(person["name"])) == True): #chinese
+        tmpName = '\\foreignlanguage{ukrainian}{' + person["name"] + "}"
+    elif (bool((re.compile(r'[\u4e00-\u9fff\u3000-\u303f\uff00-\uffef]')).search(person["name"])) == True): #"chinese"
         tmpName = '\\begin{CJK*}{UTF8}{gbsn}' + person["name"] + '\\end{CJK*}'
     else:
-        tmpName = person["name"].replace('ä', '\\"{a}').replace('ó', "\\'{o}").replace('ü', '\\"{u}').replace('ö', '\\"{o}').replace("é", "\\'{e}").replace("É", "\\'{E}").replace("á", "\\'{a}") #latex is stupid no support for this kind of characters in commands and it is way easier to do something like this in python than in latex
+        tmpName = person["name"]
     tex_builder += '\card' + '{' + tmpName + '}'
-    tex_builder += '{' + (person["wcaId"], '\\textcolor{ForestGreen}{Newcomer}')[person["wcaId"] == None] + '}'# either wcaId or 'Newcommer'
+    tex_builder += '{' + (person["wcaId"], '\\textcolor{ForestGreen}{Newcomer}')[person["wcaId"] == None] + '}'# either wcaId or 'Newcomer'
     tex_builder += '{' + pycountry.countries.get(alpha_2=person["countryIso2"]).name + '}'
     tex_builder += '{' + str(person["registrantId"]) 
     tex_builder += ('' , ('', (' \\textcolor{Red}{' + " ".join(str(x) for x in person["roles"]) + '}'))[person["roles"] != []])[print_orga_delegate == 'y'] + '}'
@@ -73,7 +67,7 @@ for person in persons:
             pass
         else:
             # acitvityId is build like event-round-group
-            # build dict (or whatever it is python there are no datatypes) 'event' : '[comp,scr,judge,run]'
+            # build dict (or whatever; it is python there are no datatypes) 'event' : '[comp,scr,judge,run]' (todo if some assignment type isn't used don't include it)
             activity = assignment['activityId'].split('-') 
             if not(activity[0] in assignments):
                 assignments[activity[0]] = [' ',' ',' ',' ']
@@ -87,10 +81,10 @@ for person in persons:
                 assignments[activity[0]][3] += ((activity[2])[1:], (',' + (activity[2])[1:]))[assignments[activity[0]][3] != ' ']
 
     # sorting
-    # assignments = {key: i for i, key in enumerate(EVENTORDER)}
+    assignmentsSorted = OrderedDict((k, assignments[k]) for k in EVENTORDER if k in assignments)
     tex_builder += '{'
-    for k in assignments:
-        tex_builder += k + '&' + str(assignments[k][0]) + '&' + str(assignments[k][1]) + '&' + str(assignments[k][2]) + '&' + str(assignments[k][3]) + '\\\\'
+    for k in assignmentsSorted:
+        tex_builder += k + '&' + str(assignmentsSorted[k][0]) + '&' + str(assignmentsSorted[k][1]) + '&' + str(assignmentsSorted[k][2]) + '&' + str(assignmentsSorted[k][3]) + '\\\\'
     tex_builder += '}'
 
 # writing to tex/content.tex
